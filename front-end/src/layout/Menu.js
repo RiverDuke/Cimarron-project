b313e60c45d7fb31167d846f25c807cbc5c54214
River <oasisnile556@gmail.com>
2022-09-03 03:34:17 -0400

    modal added

--- front-end/src/layout/Menu.js
+++ front-end/src/layout/Menu.js
@@ -11,9 +11,9 @@ import { Link } from "react-router-dom";
 function Menu() {
   return (
     <>
-      <div className="front-page-image position container-fluid d-flex flex-column align-items-center">
-        <h3 className=" mx-auto font-italic text-white display-1 text-center pt-3">
-          "Satisy that Cimarron Appitite"
+      <div className=" front-page-image position container-fluid d-flex flex-column align-items-center">
+        <h3 className=" lead mx-auto font-italic text-white display-1 text-center pt-3">
+          "Satisfy that Cimarron Appitite"
         </h3>
 
         <div className=" mt-auto mb-5">
@@ -21,10 +21,10 @@ function Menu() {
         </div>
       </div>
 
-      <div className="container">
-        <h1 className="">MENU</h1>
-        <div className="row">
-          <div class="card border-light m-2" style={{ width: "18rem" }}>
+      <div className="container menu">
+        <h1 className="d-block text-center display-2">MENU</h1>
+        <div className="row d-flex justify-content-center">
+          <div class="card border-light m-4" style={{ width: "18rem" }}>
             <img src="..." class="card-img-top" alt="..." />
             <div class="card-body">
               <h5 class="card-title">Card title</h5>
@@ -34,7 +34,7 @@ function Menu() {
               </p>
             </div>
           </div>
-          <div class="card border-light m-2" style={{ width: "18rem" }}>
+          <div class="card border-light m-4" style={{ width: "18rem" }}>
             <img src="..." class="card-img-top" alt="..." />
             <div class="card-body">
               <h5 class="card-title">Card title</h5>
@@ -44,7 +44,7 @@ function Menu() {
               </p>
             </div>
           </div>
-          <div class="card border-light m-2" style={{ width: "18rem" }}>
+          <div class="card border-light m-4" style={{ width: "18rem" }}>
             <img src="..." class="card-img-top" alt="..." />
             <div class="card-body">
               <h5 class="card-title">Card title</h5>
@@ -54,7 +54,7 @@ function Menu() {
               </p>
             </div>
           </div>
-          <div class="card border-light m-2" style={{ width: "18rem" }}>
+          <div class="card border-light m-4" style={{ width: "18rem" }}>
             <img src="..." class="card-img-top" alt="..." />
             <div class="card-body">
               <h5 class="card-title">Card title</h5>
@@ -64,7 +64,7 @@ function Menu() {
               </p>
             </div>
           </div>
-          <div class="card border-light m-2" style={{ width: "18rem" }}>
+          <div class="card border-light m-4" style={{ width: "18rem" }}>
             <img src="..." class="card-img-top" alt="..." />
             <div class="card-body">
               <h5 class="card-title">Card title</h5>
@@ -74,7 +74,7 @@ function Menu() {
               </p>
             </div>
           </div>
-          <div class="card border-light m-2" style={{ width: "18rem" }}>
+          <div class="card border-light m-4" style={{ width: "18rem" }}>
             <img src="..." class="card-img-top" alt="..." />
             <div class="card-body">
               <h5 class="card-title">Card title</h5>
@@ -82,6 +82,53 @@ function Menu() {
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
               </p>
+            </div>
+          </div>
+          <button
+            type="button"
+            class="btn btn-primary"
+            data-toggle="modal"
+            data-target="#exampleModal"
+          >
+            Launch demo modal
+          </button>
+
+          <div
+            class="modal fade"
+            id="exampleModal"
+            tabindex="-1"
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true"
+          >
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">
+                    Modal title
+                  </h5>
+                  <button
+                    type="button"
+                    class="close"
+                    data-dismiss="modal"
+                    aria-label="Close"
+                  >
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">...</div>
+                <div class="modal-footer">
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    data-dismiss="modal"
+                  >
+                    Close
+                  </button>
+                  <button type="button" class="btn btn-primary">
+                    Save changes
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
